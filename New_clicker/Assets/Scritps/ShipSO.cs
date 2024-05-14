993a9c47360b2d69c470e813c0a5650c84b5b251
ShimaiLudoed <153752429+ShimaiLudoed@users.noreply.github.com>
2024-05-14 14:13:01 +0300

    save

---
+++ New_clicker/Assets/Scritps/ShipSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "ShipMenu", menuName = "scriptableobject/new Ship UPG", order = 1)]
+public class ShipSO : ScriptableObject
+{
+    
+}
